10066a438af2bfb429f4c656bb50227b916fb918
Byungkook Jang <bkook@BK-MacBook-Pro.local>
2017-04-11 02:24:22 +0900

    Change type of `cardPatterns` to `NSArray`

--- BKMoneyKit/BKCardNumberFormatter.m
+++ BKMoneyKit/BKCardNumberFormatter.m
@@ -12,7 +12,7 @@
 
 @interface BKCardNumberFormatter ()
 
-@property (nonatomic, strong) NSSet                 *cardPatterns;
+@property (nonatomic, strong) NSArray               *cardPatterns;
 @property (nonatomic, strong) NSRegularExpression   *nonNumericRegularExpression;
 
 @property (nonatomic, strong) NSString              *cachedPrefix;
@@ -39,7 +39,7 @@
             }
         }
         
-        self.cardPatterns = [NSSet setWithArray:mutableArray];
+        self.cardPatterns = [NSArray arrayWithArray:mutableArray];
         self.nonNumericRegularExpression = [BKMoneyUtils nonNumericRegularExpression];
         self.groupSeparater = @" ";
     }
