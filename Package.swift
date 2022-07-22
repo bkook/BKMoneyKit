62cbeade048c87a14f8a48291e03132253b093bb
Tim Mikelj <timmikelj@monzo.com>
2022-07-22 09:00:11 +0100

    add spm support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "BKMoneyKit",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "BKMoneyKit",
+            targets: ["BKMoneyKit"]),
+    ],
+    targets: [
+        .target(
+            name: "BKMoneyKit"
+        )
+    ]
+)
